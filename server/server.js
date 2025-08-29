afda3c8d8175b2e21791b7b480319d20bc815f18
Pravat Yadav <yadavpravat53@gmail.com>
2025-08-29 13:21:40 +0530

    fiexed few errors in importing

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.jsx"
+import { inngest, functions } from "./inngest/index.js"
 
 const app=express();
 const port=3000;
